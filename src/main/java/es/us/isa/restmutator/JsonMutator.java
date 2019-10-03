041e8905f907b8a883a654eeea8763676d3e0234
Alberto Martín López <alberto.martin95@gmail.com>
2019-10-03 18:20:57 +0200

    Added new main class (a JSON mutator) and dummy test

---
+++ src/main/java/es/us/isa/restmutator/JsonMutator.java
@@ -0,0 +1,117 @@
+package es.us.isa.restmutator;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import es.us.isa.restmutator.operator.value.string0.StringMutator;
+
+import java.util.Iterator;
+import java.util.Map;
+
+import static es.us.isa.restmutator.util.PropertyManager.readProperty;
+
+/**
+ * Class to manage mutation of JSON objects. Also works with JSON arrays.
+ *
+ * @author Alberto Martin-Lopez
+ */
+public class JsonMutator {
+
+    private StringMutator stringMutator;
+//    private LongMutator longMutator;
+//    private DoubleMutator doubleMutator;
+//    private BooleanMutator booleanMutator;
+//    private ObjectMutator objectMutator;
+//    private ArrayMutator arrayMutator;
+
+    public JsonMutator() {
+        if (Boolean.parseBoolean(readProperty("operator.value.string.enabled")))
+            stringMutator = new StringMutator();
+//        if (Boolean.parseBoolean(readProperty("operator.value.long.enabled")))
+//            longMutator = new LongMutator();
+//        if (Boolean.parseBoolean(readProperty("operator.value.double.enabled")))
+//            doubleMutator = new DoubleMutator();
+//        if (Boolean.parseBoolean(readProperty("operator.value.boolean.enabled")))
+//            booleanMutator = new BooleanMutator();
+//        if (Boolean.parseBoolean(readProperty("operator.object.enabled")))
+//            objectMutator = new ObjectMutator();
+//        if (Boolean.parseBoolean(readProperty("operator.array.enabled")))
+//            arrayMutator = new ArrayMutator();
+    }
+
+    /**
+     * Apply some random mutations to a JSON object. These mutations are applied to sub-objects and
+     * sub-arrays recursively: add new properties, remove existing properties, mutate existing
+     * properties (numbers, strings and booleans), make existing properties null, leave existing
+     * objects and arrays empty ({} or []).
+     *
+     * @param jsonNode The object to mutate
+     * @return The mutated object
+     */
+    public JsonNode mutateJSON(JsonNode jsonNode) {
+        if (jsonNode.isObject()) {
+            Iterator<Map.Entry<String,JsonNode>> objectPropertiesIterator = jsonNode.fields();
+            Map.Entry<String,JsonNode> jsonProperty;
+            while (objectPropertiesIterator.hasNext()) { // Iterate over each property
+                jsonProperty = objectPropertiesIterator.next();
+                if (jsonProperty.getValue().isObject() || jsonProperty.getValue().isArray()) { // If property is object or array...
+                    jsonProperty.setValue(mutateJSON(jsonProperty.getValue())); // ... recursively call this function
+                } else if (jsonProperty.getValue().isNull()) { // If property is null
+
+                } else { // Otherwise mutate it
+                    mutateProperty((ObjectNode)jsonNode, jsonProperty);
+                }
+            }
+        } else if (jsonNode.isArray()) {
+            Iterator<JsonNode> arrayElements = jsonNode.elements();
+            JsonNode arrayElement;
+            int arrayIndex = -1;
+            while (arrayElements.hasNext()) { // Iterate over each element
+                arrayElement = arrayElements.next();
+                arrayIndex++;
+                if (arrayElement.isObject() || arrayElement.isArray()) { // If property is object or array...
+                    mutateJSON(arrayElement); // ... recursively call this function
+                } else if (arrayElement.isNull()) { // If property is null
+
+                } else { // Otherwise mutate it
+                    mutateProperty((ArrayNode)jsonNode, arrayIndex);
+                }
+            }
+        }
+
+        return jsonNode;
+    }
+
+    public void mutateProperty(ObjectNode objectNode, Map.Entry<String,JsonNode> jsonProperty) {
+        if (jsonProperty.getValue().isIntegralNumber()) {
+
+        } else if (jsonProperty.getValue().isFloatingPointNumber()) {
+
+        } else if (jsonProperty.getValue().isTextual()) {
+            stringMutator.mutate(objectNode, jsonProperty.getKey());
+        } else if (jsonProperty.getValue().isBoolean()) {
+
+        }
+    }
+
+    public void mutateProperty(ArrayNode arrayNode, int index) {
+        if (arrayNode.get(index).isIntegralNumber()) {
+
+        } else if (arrayNode.get(index).isFloatingPointNumber()) {
+
+        } else if (arrayNode.get(index).isTextual()) {
+            stringMutator.mutate(arrayNode, index);
+        } else if (arrayNode.get(index).isBoolean()) {
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
---
+++ src/test/java/es/us/isa/restmutator/JsonMutatorTest.java
@@ -0,0 +1,45 @@
+package es.us.isa.restmutator;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class JsonMutatorTest {
+
+    @Test
+    public void test() {
+//        System.out.println(System.getProperty("user.dir"));
+
+        // Read JSON file
+        String jsonPath = "src/test/resources/test-object.json";
+        String jsonString = "";
+        try {
+            jsonString = new String(Files.readAllBytes(Paths.get(jsonPath)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Create mutator object (optionally change default configuration with the setters)
+        JsonMutator mutator = new JsonMutator();
+//        mutator.setMutateValueProb(0.5f);
+
+        // Create JsonNode and mutate it
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            JsonNode jsonNode = objectMapper.readTree(jsonString);
+            System.out.println("Original JSON object:\n.\n.\n.\n");
+            System.out.println(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(objectMapper.readValue(jsonNode.toString(), Object.class)));
+            JsonNode mutatedJsonNode = mutator.mutateJSON(jsonNode);
+            System.out.println(".\n.\n.\nMutated JSON object:\n.\n.\n.\n");
+            System.out.println(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(objectMapper.readValue(mutatedJsonNode.toString(), Object.class)));
+        } catch (IOException e) {
+//            e.printStackTrace();
+            System.out.println("Unable to get properties, it is not formatted in JSON");
+        }
+
+    }
+}
