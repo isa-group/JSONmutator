cbed3bfc1351384d70a93c7bd96afb37d349233f
Alberto Martín López <alberto.martin95@gmail.com>
2019-10-08 14:02:41 +0200

    Added boundary string operator

--- src/main/java/es/us/isa/jsonmutator/mutator/value/string0/StringMutator.java
+++ src/main/java/es/us/isa/jsonmutator/mutator/value/string0/StringMutator.java
@@ -3,6 +3,7 @@ package es.us.isa.jsonmutator.mutator.value.string0;
 import es.us.isa.jsonmutator.mutator.AbstractMutator;
 import es.us.isa.jsonmutator.mutator.value.common.operator.ChangeTypeOperator;
 import es.us.isa.jsonmutator.mutator.value.common.operator.NullOperator;
+import es.us.isa.jsonmutator.mutator.value.string0.operator.StringBoundaryOperator;
 import es.us.isa.jsonmutator.mutator.value.string0.operator.StringMutationOperator;
 import es.us.isa.jsonmutator.mutator.value.string0.operator.StringReplacementOperator;
 import es.us.isa.jsonmutator.util.OperatorNames;
@@ -22,6 +23,7 @@ public class StringMutator extends AbstractMutator {
         prob = Float.parseFloat(readProperty("operator.value.string.prob"));
         operators.put(OperatorNames.REPLACE, new StringReplacementOperator());
         operators.put(OperatorNames.MUTATE, new StringMutationOperator());
+        operators.put(OperatorNames.BOUNDARY, new StringBoundaryOperator());
         operators.put(OperatorNames.NULL, new NullOperator(String.class));
         operators.put(OperatorNames.CHANGE_TYPE, new ChangeTypeOperator(String.class));
     }
---
+++ src/main/java/es/us/isa/jsonmutator/mutator/value/string0/operator/StringBoundaryOperator.java
@@ -0,0 +1,48 @@
+package es.us.isa.jsonmutator.mutator.value.string0.operator;
+
+import es.us.isa.jsonmutator.mutator.AbstractOperator;
+import es.us.isa.jsonmutator.util.OperatorNames;
+import org.apache.commons.lang3.RandomStringUtils;
+
+import static es.us.isa.jsonmutator.util.PropertyManager.readProperty;
+
+/**
+ * Operator that mutates a string by replacing it with a boundary value, namely
+ * an empty string, an uppercase string, a lowercase string, a string of minimum
+ * length or a string of maximum length.
+ */
+public class StringBoundaryOperator extends AbstractOperator {
+
+    private int minLength;          // Minimum length of the randomly generated string
+    private int maxLength;          // Maximum length of the randomly generated string
+    private String uppercaseString;
+    private String lowercaseString;
+
+    public StringBoundaryOperator() {
+        super();
+        weight = Float.parseFloat(readProperty("operator.value.string.weight." + OperatorNames.BOUNDARY));
+        minLength = Integer.parseInt(readProperty("operator.value.string.length.min"));
+        maxLength = Integer.parseInt(readProperty("operator.value.string.length.max"));
+        uppercaseString = readProperty("operator.value.string.uppercase");
+        lowercaseString = readProperty("operator.value.string.lowercase");
+    }
+
+    @Override
+    public Object mutate(Object element) {
+        float randomValue = rand2.nextFloat();
+
+        if (randomValue <= 1f/5) { // Empty string
+            return "";
+        } else if (randomValue <= 2f/5)  { // String of minLength
+            return RandomStringUtils.random(minLength, true, true);
+        } else if (randomValue <= 3f/5)  { // String of maxLength
+            return RandomStringUtils.random(maxLength, true, true);
+        } else if (randomValue <= 4f/5)  { // Lowercase string
+            return lowercaseString;
+        } else { // Uppercase string
+            return uppercaseString;
+        }
+    }
+
+
+}
